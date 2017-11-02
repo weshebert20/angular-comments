5d9006c94e28b645222f5d0ec1d10d28433bae71
weshebert20 <weshebert20@gmail.com>
2017-11-01 21:59:59 -0600

    first pt of homework done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,74 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+	editMode = false;
+
+	newComment = {
+		text: '',
+		author: '',
+		editMode: false
+	};
+
+
   comments = [
-      'first comment!',
-      'nice work!',
-      'I would also like to congratulate you!'
-  ];
+      	{
+      	text: 'first comment!',
+      	author: 'Wes',
+      	editMode: false
+  		},
+  		{
+      	text: 'nice work!',
+      	author: 'Brad',
+      	editMode: false
+  		},
+  		{
+      	text: 'I would also like to congratulate you!',
+      	author: 'Kelly',
+      	editMode: false
+  		},
+  ]
+
+ addComment() {
+ 	this.comments.push(this.newComment);
+ 	this.newComment = {
+ 		text: '',
+		author: '',
+		editMode: false
+ 	}    
+  }
+
+  deleteComment(comment){
+  	for(let i=0; i < this.comments.length; i++){
+  		if(this.comments[i].text === comment.text){
+  			this.comments.splice(i, 1);
+  		}
+  	}
+  }
+
+  editComment(comment){
+  		comment.editMode = true;
+  }
+
+  cancelComment(comment){
+  		comment.editMode = false;
+  }
+
+  editNow(comment){
+  	console.log(comment);
+  	for (let i =0; i < this.comments.length; i++){
+  		if (this.comments[i].text === comment.text){
+  			this.comments[i].text === comment.text;
+  			this.comments[i].author === comment.author;
+  		}
+  	}
+  	comment.editMode = false;
+  }
+
+
+
+
+
+
 }
+
